536164f1d3102b4bd0f8660efae5a252b8a75b7b
JorgeOnate <jorge.onate.2017@upb.edu.co>
2020-08-31 21:32:37 -0500

    FirstAdvancement
    This is the first advancement of the game where i simply, add the controls of the bird to fly (with click) and a few elements like the background and ground.

---
+++ Assets/Scripts/FlyMecanism.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyMecanism : MonoBehaviour
+{
+    //This Script simply controls the movement of the bird
+    //in which case it only jumps
+    // Start is called before the first frame update
+   
+    [Header("Velocity of flight:")]
+    [SerializeField] public float velocity = 1;
+    
+    //[Header("Jump Control:")]
+    //[SerializeField] private KeyCode MouseL;
+    private Rigidbody2D _rigidbody2D;
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //jump
+            _rigidbody2D.velocity = Vector2.up * velocity;
+        }
+    }
+}
