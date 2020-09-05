d3adcdff58420ce7aecfb1f991f2849993fc06b2
JorgeOnate <jorge.onate.2017@upb.edu.co>
2020-09-05 18:55:14 -0500

    AdditionOfEnemies
    I added enemies which you can click to destroy and they also give you points, if the enemies touch you , you will also die.

---
+++ Assets/Scripts/Spaceship.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship : MonoBehaviour
+{
+    [SerializeField] public float speed = 6.0f;
+
+    
+    // Simple movement of ship
+    void Start()
+    {
+        
+        
+    }
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+
+    //private void OnMouseDown()
+    //{
+    //    if (Input.GetMouseButton(0))
+    //    {
+    //        Destroy(gameObject);
+    //        Debug.Log("clear");
+    //    }
+    //}
+}
---
+++ Assets/Scripts/SpaceshipSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipSpawner : MonoBehaviour
+{
+    [SerializeField]public float maxTime = 0;
+    private float _timer = 0;
+    public GameObject spaceShip;
+    
+    [SerializeField]public float TopHeight = 0;
+    [SerializeField]public float BottomHeight = 0;
+
+    // This will create the spawning of the enemies
+    
+    void Start()
+    {
+        GameObject newspace = Instantiate(spaceShip);
+        newspace.transform.position = transform.position + new Vector3(0, Random.Range(-BottomHeight,TopHeight),0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_timer > maxTime)
+        {
+            GameObject newspace = Instantiate(spaceShip);
+            newspace.transform.position = transform.position + new Vector3(0, Random.Range(-BottomHeight,TopHeight),0);
+            
+            Destroy(newspace,15);
+            _timer = 0;
+
+        }
+
+        
+        _timer += Time.deltaTime;
+    }
+    
+    
+    
+}
